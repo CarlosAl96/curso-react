be13df7c82e58685098d8e56bc9fed8504ca2bbc
Carlos Ceballo <carlos96cacv@gmail.com>
2020-11-25 10:40:43 -0400

    clean up project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './assets/css/App.css';
 
 //Importo componentes
@@ -8,15 +7,8 @@ import MiComponente from './components/MiComponente.js';
 function App() {
 
   return (
-
-
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
         <MiComponente />
-
-      </header>
     </div>
   );
 }
